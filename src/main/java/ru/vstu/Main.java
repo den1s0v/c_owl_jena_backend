3b7e62e2844025c1be8c150e75977ad47f5b3315
den1s0v <hello-lumen@ya.ru>
2021-11-29 19:50:05 +0300

    Rule sets (files) sequencing

--- src/main/java/ru/vstu/Main.java
+++ src/main/java/ru/vstu/Main.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -27,7 +28,7 @@ public class Main {
             System.out.println("Please provide (the optional mode and) 3 commandline arguments:\n" +
                     " 0) Mode to run in: 'jena' (the default) or 'sparql'\n" +
                     " 1) Path to input RDF file\n" +
-                    " 2) Path to Jena rules file\n" +
+                    " 2) Path to Jena rules file (for several files separated with ';' their rule sets executed sequentially)\n" +
                     " 3) Path to location where to store the output N-Triples file\n" +
                     ""
             );
@@ -87,31 +88,50 @@ public class Main {
     }
 
     public static void runReasoning(String in_rdf_url, String rules_path, String out_rdf_path) {
+
+        ArrayList<String> rules_paths = new ArrayList();
+        if (rules_path.contains(";")) {
+            String[] fnms = rules_path.split(";");
+            for (String fnm : fnms)
+                rules_paths.add(fnm);
+        } else {
+            rules_paths.add(rules_path);
+        }
+        rules_path = rules_paths.get(0);
+
         // Register a namespace for use in the rules
 //        String baseURI = "http://vstu.ru/poas/ctrl_structs_2020-05_v1#";
 //        String baseURI = "http://penskoy.n/expressions1#
         String baseURI = findIriForPrefix(rules_path, "my");
         PrintUtil.registerPrefix("my", baseURI);
 
-        List<Rule> rules = Rule.rulesFromURL(rules_path);
         Model data = RDFDataMgr.loadModel(in_rdf_url);
 
-        GenericRuleReasoner reasoner = new GenericRuleReasoner(rules);
-//        reasoner.setOWLTranslation(true);               // not needed in RDFS case
-//        reasoner.setTransitiveClosureCaching(true);     // not required when there is no use of transitivity
+        long startTimeWhole = System.nanoTime();
+        for (String curr_rules_path : rules_paths)
+        {
+            List<Rule> rules = Rule.rulesFromURL(curr_rules_path);
 
-        long startTime = System.nanoTime();
+            GenericRuleReasoner reasoner = new GenericRuleReasoner(rules);
+    //        reasoner.setOWLTranslation(true);               // not needed in RDFS case
+    //        reasoner.setTransitiveClosureCaching(true);     // not required when there is no use of transitivity
 
-        InfModel inf = ModelFactory.createInfModel(reasoner, data);
-        inf.prepare();
+            long startTime = System.nanoTime();
 
-        long estimatedTime = System.nanoTime() - startTime;
-        System.out.println("Time spent on reasoning: " + String.valueOf((float)(estimatedTime / 1000 / 1000) / 1000) + " seconds.");
+            InfModel inf = ModelFactory.createInfModel(reasoner, data);
+            inf.prepare();
+            data = inf;
+
+            long estimatedTime = System.nanoTime() - startTime;
+            System.out.println("Time spent on reasoning: " + String.valueOf((float)(estimatedTime / 1000 / 1000) / 1000) + " seconds.");
+        }
+        long estimatedTime = System.nanoTime() - startTimeWhole;
+        System.out.println("Time spent on all reasoning steps: " + String.valueOf((float)(estimatedTime / 1000 / 1000) / 1000) + " seconds.");
 
         FileOutputStream out = null;
         try {
             out = new FileOutputStream(out_rdf_path);
-            RDFDataMgr.write(out, inf, Lang.NTRIPLES);  // Lang.NTRIPLES  or  Lang.RDFXML
+            RDFDataMgr.write(out, data, Lang.NTRIPLES);  // Lang.NTRIPLES  or  Lang.RDFXML
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
