afada09c1d3648110d50821d549b63d26acbe228
den1s0v <hello-lumen@ya.ru>
2022-04-09 22:41:34 +0300

    Checkpointer: improve measuring precision

--- src/main/java/ru/vstu/util/Checkpointer.java
+++ src/main/java/ru/vstu/util/Checkpointer.java
@@ -2,14 +2,14 @@ package ru.vstu.util;
 
 public class Checkpointer {
     //        'Measures time between hits. Requires the `from timeit import default_timer as timer`'
-    float first, last;
+    double first, last;
 
     public Checkpointer() {
         reset_now();
     }
 
-    public float timer() {
-        return (float) (System.nanoTime() / 1000000) / 1000;
+    public double timer() {
+        return (double) (System.nanoTime() / 1000) / 1000000;
     }
 
     public void reset_now() {
@@ -17,19 +17,19 @@ public class Checkpointer {
         this.last = this.first;
     }
 
-    public float hit(String label) {
+    public double hit(String label) {
 
-        float now = timer();
-        float delta = now - this.last;
+        double now = timer();
+        double delta = now - this.last;
         if (label != null)
             System.out.println((!label.isEmpty() ? label : "Checkpoint") + ": " + String.format("%.3f", delta) + "s");
         this.last = now;
         return delta;
     }
 
-    public float since_start(String label, boolean hit) {
-        float now = timer();
-        float delta = now - this.first;
+    public double since_start(String label, boolean hit) {
+        double now = timer();
+        double delta = now - this.first;
         if (label != null)
             System.out.println((!label.isEmpty() ? label : "Total") + ": " + String.format("%.3f", delta) + "s");
         if (hit)
