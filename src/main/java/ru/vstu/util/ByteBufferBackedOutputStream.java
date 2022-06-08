e5c8dae2b358fddb621533fb59b68a28254fcfd5
den1s0v <hello-lumen@ya.ru>
2022-06-08 19:36:23 +0300

    Update Jena libs to version 4.5.0

---
+++ src/main/java/ru/vstu/util/ByteBufferBackedOutputStream.java
@@ -0,0 +1,55 @@
+package ru.vstu.util;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.Objects;
+
+public class ByteBufferBackedOutputStream extends OutputStream
+{
+    private ByteBuffer backendBuffer;
+
+    public ByteBufferBackedOutputStream(ByteBuffer backendBuffer) {
+        Objects.requireNonNull(backendBuffer, "Given backend buffer can not be null!");
+        this.backendBuffer = backendBuffer;
+    }
+
+    public void close() throws IOException {
+        this.backendBuffer = null;
+    }
+
+    private void ensureStreamAvailable() throws IOException {
+        if (this.backendBuffer == null) {
+            throw new IOException("write on a closed OutputStream");
+        }
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        this.ensureStreamAvailable();
+        backendBuffer.put((byte) b);
+    }
+
+    @Override
+    public void write(@NotNull byte[] bytes) throws IOException {
+        this.write(bytes, 0, bytes.length);
+    }
+
+    @Override
+    public void write(@NotNull byte[] bytes, int off, int len) throws IOException {
+        this.ensureStreamAvailable();
+        Objects.requireNonNull(bytes, "Given buffer can not be null!");
+        if ((off < 0) || (off > bytes.length) || (len < 0) ||
+                ((off + len) > bytes.length) || ((off + len) < 0))
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        else if (len == 0) {
+            return;
+        }
+
+        backendBuffer.put(bytes, off, len);
+    }
+}
